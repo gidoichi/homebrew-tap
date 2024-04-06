67b90a508d25759b28c39b55c9bdeb9f5657bd6a
Shinichi Yanagido <hakuto1yds@gmail.com>
2024-04-06 17:52:32 +0900

    add yaml-path.rb

---
+++ Formula/y/yaml-path.rb
@@ -0,0 +1,13 @@
+class YamlPath < Formula
+  desc 'Get "xpath" for a given line at column of a YAML file'
+  homepage "https://github.com/gidoichi/yaml-path"
+  url "https://github.com/gidoichi/yaml-path/archive/refs/tags/v1.4.2.tar.gz"
+  sha256 "9177095aefd4c0f90ffed93d3b8c5489a4858b9dcafbf247971197e9a561f9d0"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/gidoichi/yaml-path/presentation/cli.version=v#{version}")
+  end
+end
