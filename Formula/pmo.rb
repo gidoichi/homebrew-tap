0e5c2503216f419cb05abdf96a956167635fe0c7
gidoichi <hakuto1yds@gmail.com>
2024-10-20 07:02:38 +0900

    persistence config on pmo

--- Formula/pmo.rb
+++ Formula/pmo.rb
@@ -8,5 +8,27 @@ class Pmo < Formula
   def install
     libexec.install Dir["*"]
     bin.write_exec_script libexec/"bin/pmo"
+
+    ohai "To create a configuration directory to persist your data, run `brew postinstall #{name}`"
+  end
+
+  def post_install
+    if ENV.include?("XDG_CONFIG_HOME")
+      config = "#{ENV["XDG_CONFIG_HOME"]}/pmo-gidoichi"
+    else
+      config = "#{ENV["HOME"]}/.config/pmo-gidoichi"
+    end
+
+    if Dir.exist?(config)
+      ohai "The configuration directory already exists. Run `rm -rf #{config}/*` to install cleanly."
+    end
+
+    FileUtils.mkdir_p("#{config}/config")
+    unless Dir.exist?(libexec/"config")
+      FileUtils.ln_s("#{config}/config", libexec/"config")
+    end
+    unless Dir.exist?("#{config}/managers")
+      FileUtils.ln_s(libexec/"managers", "#{config}/managers")
+    end
   end
 end
